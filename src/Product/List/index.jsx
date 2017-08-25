cae2892a5a964265f1548d096562028aaa5d20f4
ilia.batyrev <ilya.batyrev@gmail.com>
2017-08-25 14:52:09 +0300

    fix: add list content wrapper

--- src/Product/List/index.jsx
+++ src/Product/List/index.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Helmet } from 'react-helmet';
 
 import Categories from '../Categories';
 import CategoryHeader from './CategoryHeader';
 import SubCategory from './SubCategory';
 import ShowMore from './ShowMore';
+
+const ListContent = styled.section`
+  position: relative;
+
+  &::before {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, .3);
+    z-index: -10;
+    opacity: 0;
+    transition: all .15s ease-in-out;
+  }
+
+  &.is-opened::before {
+    opacity: 1;
+    z-index: 10;
+  }
+`;
 
 export default () =>
   (<div>
@@ -15,9 +39,11 @@ export default () =>
     <Categories />
     <CategoryHeader />
 
-    <div className="container">
-      <SubCategory />
+    <ListContent>
+      <div className="container">
+        <SubCategory />
 
-      <ShowMore />
-    </div>
+        <ShowMore />
+      </div>
+    </ListContent>
   </div>);
